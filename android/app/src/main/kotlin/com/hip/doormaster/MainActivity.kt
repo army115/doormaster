4b2ac8461b6b3ec47955b7cde17b7e50593cf81e
arm115 <sriwongsaeng115@gmail.com>
2023-04-04 16:28:57 +0700

    update ios

---
+++ android/app/src/main/kotlin/com/hip/doormaster/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hip.doormaster
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,13 @@
-#import "AppDelegate.h"
-#import "GeneratedPluginRegistrant.h"
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCtHPbBuAM2ktRSLYB07NADmKUIrYVpqPI"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
